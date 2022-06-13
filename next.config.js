f3f11b89842a140dcdc38e1aa77fca413b7996c7
PRigas96 <93339610+PRigas96@users.noreply.github.com>
2022-06-13 14:04:25 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  ignoreDuringBuilds: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   reactStrictMode: false,
   swcMinify: false
 }
